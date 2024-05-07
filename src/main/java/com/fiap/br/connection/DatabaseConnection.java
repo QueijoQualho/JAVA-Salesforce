b2426dafe901b622af25b89cc4ec1c4a5f4d64c1
Pedromra.04@gmail.com <logonrmlocal@fiap.com.br>
2024-05-07 11:22:59 -0300

    add PlanoController

--- src/main/java/com/fiap/br/connection/DatabaseConnection.java
+++ src/main/java/com/fiap/br/connection/DatabaseConnection.java
@@ -1,40 +1,21 @@
 package com.fiap.br.connection;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class DatabaseConnection {
     private static Connection connection;
-    public static String URL_ORACLE;
-    public static String USER;
-    public static String PASSWORD;
-
-    static {
-        try (InputStream inputStream = DatabaseConnection.class.getClassLoader()
-                .getResourceAsStream("database.properties")) {
-            Properties properties = new Properties();
-            properties.load(inputStream);
-            URL_ORACLE = properties.getProperty("jdbc.url");
-            USER = properties.getProperty("jdbc.username");
-            PASSWORD = properties.getProperty("jdbc.password");
-            System.out.println(URL_ORACLE);
-            System.out.println(USER);
-            System.out.println(PASSWORD);
-        } catch (IOException e) {
-            throw new RuntimeException("Falha ao carregar as propriedades do banco de dados", e);
-        }
-    }
+    private static final String url = "jdbc:oracle:thin:@//oracle.fiap.com.br:1521/ORCL";
+    private static final String username = "RM553912";
+    private static final String password = "141204";
 
     private DatabaseConnection() {
     }
 
     public static Connection getConnection() {
         try {
-            if (connection == null || connection.isClosed() || !connection.isValid(5)) {
+            if (connection == null || connection.isClosed()) {
                 createConnection();
             }
         } catch (SQLException e) {
@@ -46,10 +27,9 @@ public class DatabaseConnection {
 
     private static void createConnection() {
         try {
-            connection = DriverManager.getConnection(URL_ORACLE, USER, PASSWORD);
+            connection = DriverManager.getConnection(url, username, password);
         } catch (SQLException ex) {
             throw new RuntimeException("Erro ao conectar ao banco de dados.", ex);
         }
     }
-
 }
